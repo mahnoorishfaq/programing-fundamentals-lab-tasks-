066dfa657b4caddff4bec5b08595ff728d06cbcf
Mahnoor Ishfaq <164208590+mahnoorishfaq@users.noreply.github.com>
2026-08-26 22:13:44 +0500

    Modify while loop
    Revised while loop to iterate from 1 to 5 instead of 1 to 10.

--- while loop.cpp
+++ while loop.cpp
@@ -1,13 +1,6 @@
 #include <iostream> 
 using namespace std; 
 main()
-//{ 
-//   int x=1; 
-//   while ( x <= 10 )
-//   {  
-//      cout<<"2"<<"*"<<x<<"="<<x*2<<endl; 
-//      x++; 
-//   } 
  {
  int x,y, sum;
   while (x<=5)
